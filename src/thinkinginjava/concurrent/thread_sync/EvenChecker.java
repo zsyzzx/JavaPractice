9b100b321c3e29b805a3fa7ccfc5725f54f758c9
seven <zsyzzx@foxmail.com>
2018-10-04 15:53:35 +0800

    修改代码，添加注释 并发错误：共享资源竞争问题

--- src/thinkinginjava/concurrent/thread_sync/EvenChecker.java
+++ src/thinkinginjava/concurrent/thread_sync/EvenChecker.java
@@ -5,6 +5,9 @@ import java.util.concurrent.Executors;
 
 /**
  * Created by seven on 2017/9/6.
+ * Checker类通过创建线程同时多次访问同一Generator对象,并进行测试,如果出现失败,就停止该任务并返回
+ *
+ * 解决资源竞争问题: 一个任务不能依赖另一个任务,因为任务关闭的顺序无法得到保证
  */
 public class EvenChecker implements Runnable {
     private IntGenerator generator;
@@ -26,6 +29,8 @@ public class EvenChecker implements Runnable {
         }
     }
 
+
+//    测试所有的数字生成器
     public static void test(IntGenerator gp, int count){
         System.out.println("test");
         ExecutorService exec = Executors.newCachedThreadPool();
--- src/thinkinginjava/concurrent/thread_sync/EvenGenerator.java
+++ src/thinkinginjava/concurrent/thread_sync/EvenGenerator.java
@@ -2,6 +2,9 @@ package thinkinginjava.concurrent.thread_sync;
 
 /**
  * Created by seven on 2017/9/7.
+ * 该类线程不安全
+ * 解决方法: 实现线程安全
+ * 互斥区方法,手动加锁{@link MutexEvenGenerator}, synchronized关键字方法{@link SynchronizedEvenGenerator}
  */
 public class EvenGenerator extends IntGenerator {
     private int currentEvenValue = 0;
